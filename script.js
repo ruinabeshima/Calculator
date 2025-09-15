8b35e58c4b0c23263228c3569a1b9f448d986956
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-09-15 13:46:12 +0900

    Added basic operation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(value1, value2){
+  let result = value1 + value2
+  return result
+}
+
+function subtract(value1, value2){
+  let result = value1 - value2
+  return result 
+}
+
+function multiply(value1, value2){
+  let result = value1 * value2
+  return result 
+}
+
+function divide(value1, value2){
+  let result = value1 / value2
+  return result 
+}
