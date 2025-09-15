75b085a6bfbfe865a3b825b5199c1460da297896
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-09-15 13:50:28 +0900

    Added function for operation

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function divide(value1, value2){
   let result = value1 / value2
   return result 
 }
+
+function operate(value1, value2, operation){
+  if (operation === "+"){
+    result = add(value1, value2)
+  } else if (operation === "-"){
+    result = subtract(value1, value2)
+  } else if (operation === "*"){
+    result = multiply(value1, value2)
+  } else if (operation === "/"){
+    result = divide(value1, value2)
+  }
+  return result
+} 
+
+//main
+let number1 
+let number2 
+let operation 
+console.log(operate(1, 2, "+"))
+console.log(operate(6, 7, "*"))
